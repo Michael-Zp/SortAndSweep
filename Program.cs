e836d7fc57672fe7717a5f45e458253b8fdb003f
Michael Zappe <mz-171742@AP053108>
2019-08-19 15:25:57 +0200

    First implementation

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using SortAndSweepTypes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SortAndSweep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SASItem> entites = new List<SASItem>();
+
+
+            Random rand = new Random();
+
+            Func<double> nextRand = ( () => rand.NextDouble() * 1000 - 500);
+
+            for(int i = 0; i < 500000; i++)
+            {
+                double minValue = nextRand();
+                double maxValue = nextRand();
+
+                if(minValue > maxValue)
+                {
+                    var temp = maxValue;
+                    maxValue = minValue;
+                    minValue = temp;
+                }
+
+                entites.AddRange(SASItem.CreateItem(minValue, maxValue));
+            }
+            
+
+            List<SASItem> xSorted = entites.OrderBy(x => x.Value).ToList();
+            
+            
+            Dictionary<int, SASItem> activeItems = new Dictionary<int, SASItem>((int)(entites.Capacity * 0.7));
+            List<SASItem> collidingItems = new List<SASItem>();
+            
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            foreach (var item in xSorted)
+            {
+                if(item.Type == MarkerType.Min)
+                {
+                    //Console.WriteLine("Added : " + item.Value);
+                    activeItems.Add(item.Id, item);
+                }
+                else
+                {
+                    //Console.WriteLine("Removed : " + item.MinItem.Value + ", " + item.Value);
+                    activeItems.Remove(item.Id);
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortAndSweepTypes.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace SortAndSweepTypes
+{
+
+    public enum MarkerType
+    {
+        Min, Max
+    }
+    public class Marker
+    {
+        public float Value { get; set; }
+        public MarkerType Type { get; set; }
+
+        public Marker(float value, MarkerType type)
+        {
+            Value = value;
+            Type = type;
+        }
+    }
+
+
+
+    class SASItem
+    {
+        public float Value;
+        public MarkerType Type;
+        public readonly int Id;
+
+        public SASItem MinItem;
+        
+        private static int _nextId { get; set; }
+        
+        private readonly Marker _marker;
+        
+        public void UpdateValues()
+        {
+            Value = _marker.Value;
+        }
+
+        private static List<SASItem> CreateItem(Marker minMarker, Marker maxMarker)
+        {
+            int Id = _nextId;
+            _nextId++;
+
+            SASItem minItem = new SASItem(minMarker, Id);
+
+
+            return new List<SASItem>() { minItem, new SASItem(maxMarker, Id, minItem) };
+        }
+
+        private SASItem(Marker marker, int id)
+        {
+            Id = id;
+
+            _marker = marker;
+            Type = marker.Type;
+            MinItem = this;
+
+            UpdateValues();
+        }
+
+        private SASItem(Marker marker, int id, SASItem minItem) : this(marker, id)
+        {
+            MinItem = minItem;
+        }
+
+        public static List<SASItem> CreateItem(float min, float max)
+        {
+            return CreateItem(new Marker(min, MarkerType.Min), new Marker(max, MarkerType.Max));
+        }
+
+        public static List<SASItem> CreateItem(double min, double max)
+        {
+            return CreateItem(new Marker((float)min, MarkerType.Min), new Marker((float)max, MarkerType.Max));
+        }
+    }
+
+
+    //class MySASList
+    //{
+    //    public MySASListItem First;
+    //    public MySASListItem Last;
+
+    //    public void AddLast(SASItem item)
+    //    {
+    //        if(First == null)
+    //        {
+    //            MySASListItem listItem = new MySASListItem(null, item, this);
+    //            First = listItem;
+    //            Last = listItem;
+    //        }
+    //        else
+    //        {
+    //            MySASListItem listItem = new MySASListItem(Last, item, this);
+    //            Last.Next = listItem;
+    //        }
+    //    }
+    //}
+
+    //class MySASListItem
+    //{
+    //    public MySASListItem Before;
+    //    public SASItem This;
+    //    public MySASListItem Next;
+
+    //    public MySASList List;
+
+    //    public MySASListItem(MySASListItem before, SASItem @this, MySASList list)
+    //    {
+    //        Before = before;
+
+    //        if (Before != null)
+    //        {
+    //            Before.Next = this;
+    //        }
+
+    //        This = @this;
+    //        Next = null;
+
+    //        List = list;
+    //    }
+
+    //    public void RemoveThis()
+    //    {
+    //        if (Before != null)
+    //        {
+    //            Before.Next = Next;
+    //        }
+
+    //        if (Next != null)
+    //        {
+    //            Next.Before = Before;
+    //        }
+
+    //        if(this == List.First)
+    //        {
+    //            List.First = Next;
+    //        }
+
+    //        if(this == List.Last)
+    //        {
+    //            List.Last = Before;
+    //        }
+    //    }
+    //}
+}
